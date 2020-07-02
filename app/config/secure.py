02304515d13bf8c94369bf1c1cd1e8ef0389bdf8
syklinux <954810908@qq.com>
2020-07-02 19:21:58 +0800

    Update secure.py

--- app/config/secure.py
+++ app/config/secure.py
@@ -1,18 +1,4 @@
 
-# import os
-#
-# basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-#
-# class Config(object):
-#     SECRET_KEY = '\xe4\xf5\xd7F\xf6\xb3kMr"\xa5\xff\t\xa1}\xb2\x00\xe0\x01\xf3\xd5\xce@6'
-#
-# class DevConfig(Config):
-#     DEBUG = True
-#     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Syk_77283720@120.78.171.189:3306/flask_test"
-#
-# config_map = {
-#     'development': DevConfig,
-# }
 
 
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:xxx@xxx.xx.xx.xxx:3306/flask_test"
